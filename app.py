8a6fbfcc1b1ca811a2b8014d2c922a707b6cb092
ADITYA'S MAC <adityasmac@ADITYASs-MacBook-Air.local>
2026-01-31 11:37:59 +0530

    Initial commit: Flask ML deployment

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+# Load the trained model
+model_path = 'medical_insurance_model.pkl'
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Extract data from form
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    
+    # Make prediction
+    prediction = model.predict(final_features)
+    output = 'class0' if prediction[0] == 1 else 'class1'
+
+    return render_template('index.html', prediction_text='Prediction: {}'.format(output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
